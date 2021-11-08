a1d980cb542cd1873cfae51e29867f571fce07e6
Michael Liu <apple@Michaels-MacBook-Air.local>
2021-11-08 18:49:09 -0500

    added Iconography Component

--- libs/demo-ui/src/lib/demo-ui.module.ts
+++ libs/demo-ui/src/lib/demo-ui.module.ts
@@ -2,7 +2,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoUIRoutingModule } from './demo-ui-routing.module';
 
-
 @NgModule({
   imports: [
     CommonModule,
