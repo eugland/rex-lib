6b55a25f01b0d245db72bfbf7aa2af28a7c7f0da
Eugene Wang <yujing.wang@ontariohealth.ca>
2021-11-16 15:10:19 -0500

    check point

--- libs/demo-ui/src/lib/typeaheads/typeaheads.component.ts
+++ libs/demo-ui/src/lib/typeaheads/typeaheads.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
-import { SIMPLE_TYPEAHEAD_DATA, ITEMTEMPLATE_DATA, GROUPING_DATA } from './typeaheads.data';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { noop, Observable, Observer, of } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { GROUPING_DATA, ITEMTEMPLATE_DATA, SIMPLE_TYPEAHEAD_DATA } from './typeaheads.data';
 import { isBs3 } from 'ngx-bootstrap/utils';
 
 
 @Component({
   selector: 'rex-lib-typeaheads',
   templateUrl: './typeaheads.component.html',
-  styleUrls: ['./typeaheads.component.scss'],
+  styleUrls: ['./typeaheads.component.scss']
 })
-export class TypeaheadsComponent {
+export class TypeaheadsComponent implements OnInit {
   model = '';
   simple_typeahead_list = SIMPLE_TYPEAHEAD_DATA;
   itemTemplate_list = ITEMTEMPLATE_DATA;
@@ -23,25 +24,75 @@ export class TypeaheadsComponent {
   list_typeahead_selected = '';
   grouping_typeahead_selected = '';
   onthefly_grouping_typeahead_selected = '';
+  remote_typeahead_selected = '';
 
+  errorMessage?: string;
+  suggestions$?: Observable<any>;
+
+  remote_short_typeahead_selected = ''
+  suggestions_short$?: Observable<any>
   isBs3 = isBs3();
 
-  convertToGroups = (list: string[]) => list
-    .map((item) => ({name: item, group: `Start with '${item.charAt(0).toUpperCase()}':` }));
+  constructor(private http: HttpClient) {
 
-  filterState = (text$: Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map((searchTerm) =>
-        searchTerm.length < 2
-          ? []
-          : this.simple_typeahead_list
-              .filter(
-                (fromList) =>
-                  fromList.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
-              )
-              .slice(0, 10)
-      )
+  }
+
+  ngOnInit() {
+    this.suggestions$ = this.getSuggestions();
+    this.suggestions_short$ = this.get_suggestions_short();
+  }
+
+  convertToGroups = (list: string[]) => list
+    .map((item) => ({ name: item, group: `Start with '${item.charAt(0).toUpperCase()}':` }));
+
+
+  getSuggestions = () => {
+    return new Observable((observer: Observer<string | undefined>) => {
+      observer.next(this.remote_typeahead_selected);
+    }).pipe(
+      switchMap((query: string) => {
+        if (query) {
+          return this.http.get<any>(
+            'https://api.geoapify.com/v1/geocode/search', {
+              params: {
+                apiKey: '8bbc9ec6b258472c994277633c3643de',
+                text: query
+              }
+            }).pipe(
+            map((data: any) => data && data.features || []),
+            tap((data) => noop, err => {
+              // in case of http error
+              this.errorMessage = err && err.message || 'Something goes wrong';
+            })
+          );
+        }
+        return of([]);
+      })
     );
+  };
+
+  get_suggestions_short = () => {
+    return new Observable((observer: Observer<string | undefined>) => {
+      observer.next(this.remote_typeahead_selected);
+    }).pipe(
+      switchMap((query: string) => {
+        if (query) {
+          return this.http.get<any>(
+            'https://en.wikipedia.org/w/api.php', {
+              params: {
+                apiKey: '8bbc9ec6b258472c994277633c3643de',
+                text: query
+              }
+            }).pipe(
+            map((data: any) => data && data.features || []),
+            tap((data) => noop, err => {
+              // in case of http error
+              this.errorMessage = err && err.message || 'Something goes wrong';
+            })
+          );
+        }
+        return of([]);
+      })
+    );
+  };
 }
