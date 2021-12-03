7c6f3d2af2b13d5d11f4d56b994edd1579bab507
michael.liu <apple@Michaels-MacBook-Air.local>
2021-12-03 16:12:39 -0500

    fixed the build issue

--- libs/one-ui/src/index.ts
+++ libs/one-ui/src/index.ts
@@ -2,4 +2,6 @@ export * from './lib/models/stepper.models';
 export * from './lib/stepper-step/stepper-step.component';
 export * from './lib/stepper/stepper.component';
 export * from './lib/datepicker/datepicker.component';
+export * from './lib/timepicker/timepicker.component';
 export * from './lib/one-ui.module';
+
